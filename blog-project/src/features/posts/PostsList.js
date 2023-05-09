b13ce91ca042fef104fbe3cf6b44501e0b990adf
codewithmawais <connectwithmawais>
2023-05-09 18:11:34 +0500

    Implemented a simple Blog project with CRUD operations in React with Redux Toolkit and Aysnc Thunk middleware.

---
+++ blog-project/src/features/posts/PostsList.js
@@ -0,0 +1,32 @@
+import { useSelector } from 'react-redux';
+import { selectAllPosts, getPostsStatus, getPostsError  } from './postsSlice';
+import PostsExcerpt from './PostsExcerpt';
+
+const PostsList = () => {
+    const posts = useSelector(selectAllPosts);
+    const postsStatus = useSelector(getPostsStatus);
+    const error = useSelector(getPostsError); 
+
+    let content;
+
+    if (postsStatus === 'loading') {
+        content = <p>"Loading..."</p>;
+    }
+    else if (postsStatus === 'succeeded') {
+        const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
+        content = orderedPosts.map((post, index) => {
+            return (<PostsExcerpt key={post.id} post={post} />)
+        })
+    }
+    else if (postsStatus === 'failed') {
+        content = <p>{error}</p>
+    }
+
+    return (
+        <section>
+            {content}
+        </section>
+    )
+};
+
+export default PostsList;
